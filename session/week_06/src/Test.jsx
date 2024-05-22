98d2ee078308e279462d86576fb46f4a6de939b0
nakyeong <yunna3425@naver.com>
2024-05-22 13:03:05 +0900

    feat: axios 기초

--- session/week_06/src/Test.jsx
+++ session/week_06/src/Test.jsx
@@ -4,25 +4,25 @@ import { Map } from "react-kakao-maps-sdk"
 
 const Test = () => {
 
-    // const [change, setChange] = useState('보내기 전')
+    const [change, setChange] = useState('보내기 전')
     
-    // const Changebutton = () => {
-    //     axios.get('https://httpbin.org/get')
-    //     .then((response) => {
-    //         if(response.status === 200){
-    //             setChange('연결이 성공하였습니다.')
-    //         }
-    //     })
-    //     .catch((error) => {
-    //         console.log(error)
-    //         setChange('연결이 실패하였습니다.')
-    //     })
-    // }
+    const Changebutton = () => {
+        axios.get('https://httpbin.org/get')
+        .then((response) => {
+            if(response.status === 200){
+                setChange('연결이 성공하였습니다.')
+            }
+        })
+        .catch((error) => {
+            console.log(error)
+            setChange('연결이 실패하였습니다.')
+        })
+    }
     
     return (
         <div className='btn_box'>
-            {/* <p>{change}</p>            
-            <button onClick={() => {Changebutton()}}>Axios</button> */}
+            <p>{change}</p>            
+            <button onClick={() => {Changebutton()}}>Axios</button>
 
             <Map // 지도를 표시할 Container
                 id="map"
