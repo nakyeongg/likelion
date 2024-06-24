998f112a0eddba8ef39b03b1a1568b337201da08
nakyeong <yunna3425@naver.com>
2024-06-24 22:19:46 +0900

    feat: 검색 및 유튜브 API 설정

--- webs-youtube/src/components/section/Search.jsx
+++ webs-youtube/src/components/section/Search.jsx
@@ -1,6 +1,17 @@
-import React from 'react'
+import React,{useEffect,useState} from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Search = () => {
+    const [searchKeyword, setSearchKeyword] = useState('');
+    const navigate = useNavigate();
+
+    const handleSearch = () => {
+        console.log(searchKeyword)
+        if (searchKeyword) {
+            navigate(`/search/${searchKeyword}`);
+            setSearchKeyword('');
+        }
+    };
   return (
     <div id='search'>
       <div className='search__inner'>
@@ -11,7 +22,14 @@ const Search = () => {
         id='searchInput'
         placeholder='검색어를 입력해주세요.'
         autoComplete='off'
-        className='search__input' />
+        className='search__input' 
+        onChange={e => setSearchKeyword(e.target.value)}
+        onKeyDown={e => {
+            if (e.key === 'Enter') {
+                handleSearch();
+            }
+        }}
+        />
       </div>
     </div>
   )
