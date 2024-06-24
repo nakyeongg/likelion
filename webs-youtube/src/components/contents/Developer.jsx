3cf0504f873ed6615a031c7fb15ec149a2b2c974
nakyeong <yunna3425@naver.com>
2024-06-24 17:16:59 +0900

    feat: 추천 개발자 작업

--- webs-youtube/src/components/contents/Developer.jsx
+++ webs-youtube/src/components/contents/Developer.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
+import { developerText } from '../../data/developer' 
+import { Link } from 'react-router-dom'
 
 const Developer = () => {
   return (
-    <div>
-      Developer
-    </div>
+    <section id='developer'>
+        <h2>추천 개발자를 소개합니다.</h2>
+        <div className='developer__inner overflow'>
+            {developerText.map((developer,key)=>(
+                <div className='developer' key={key}>
+                    <div className='developer__img play__icon'>
+                        <Link to={`/channel/${developer.channelld}`}>
+                            <img src={developer.img} alt={developer.name} />
+                        </Link>
+                    </div>
+                    <div className='developer__info'>
+                        <Link to={`/channel/${developer.channelld}`}>
+                            {developer.name}
+                        </Link>
+                    </div>
+                </div>                
+            ))}
+        </div>
+    </section>
   )
 }
 
